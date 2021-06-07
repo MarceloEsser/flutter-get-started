8ccdd4083361ab7e2b635799ff4a2390ffcca81e
Marcelo Esser <marcelo.v.esser@gmail.com>
2021-06-07 15:44:52 -0300

    starting the project

---
+++ lib/repository/http/web_client.dart
@@ -0,0 +1,11 @@
+import 'package:http/http.dart';
+import 'package:http_interceptor/http_client_with_interceptor.dart';
+
+import 'interceptors/logging_interceptor.dart';
+
+const String baseUrl = "http://demo0069341.mockable.io";
+
+final Client client = HttpClientWithInterceptor.build(
+  interceptors: [LoggingInterceptor()],
+  requestTimeout: Duration(seconds: 10),
+);
